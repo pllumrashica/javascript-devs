8650507d9f56462b3a47c75f183c3865c722980c
Pellumb Rashica <pellumbrashica1@gmail.com>
2024-01-13 11:12:00 +0100

    strings completed

---
+++ JavaScript-Classwork/numbers/script.js
@@ -0,0 +1,1 @@
+// per qdo method te stringut qe i kena msu me i bo nga 4 shembuj
--- JavaScript-Classwork/strings/exercise.js
+++ JavaScript-Classwork/strings/exercise.js
@@ -38,4 +38,60 @@ let lowerCase = "HELLO";
 console.log(lowerCase.toLowerCase());
 
 // includes css into js
-document.write("<p style='color: red;'>Hello</p>")
+// document.write("<p style='color: red;'>Hello</p>");
+
+// repeat
+let repeatString = "hello";
+let whiteSpace = " ";
+let concatStrings = repeatString.concat(whiteSpace);
+
+console.log(concatStrings.repeat(3));
+
+// replace
+let replaceString = "hello there";
+console.log(replaceString);
+console.log(replaceString.replace("world", "there"));
+
+// slice
+let sliceString = "character";
+console.log(sliceString.slice(2));
+console.log(sliceString.slice(2, 6));
+console.log(sliceString);
+
+// trim , remove empty spaces
+let trimString = "  hello  ";
+console.log(trimString.trim());
+
+let trimStartString = "  hello  ";
+console.log(trimString.trimStart());
+
+let trimEndString = "  hello  ";
+console.log(trimString.trimEnd());
+
+// regex it's an rule that we write in JavaScript for inputs to validate them.
+
+// match,regex
+let matchRegex = "the rain in spain stays mainly in the plain";
+console.log(matchRegex.match(/ain/g));
+
+// TASK TWO
+let userEmail = "pllumi@gmail.com";
+if (userEmail.slice(6, 16) == "@gmail.com") {
+    console.log("You put the correct email");
+} else {
+    console.log("You didn't put the correct email");
+}
+
+let emailWithoutSpace = "pellumbrashica@gmail.com ";
+if(emailWithoutSpace.includes(" ")) {
+    console.log(emailWithoutSpace.trimEnd());
+} else {
+    console.log("you have an white space somewhere");
+}
+
+let password = "Pllumi123";
+if(password.match(/P/g)) {
+    console.log("You are logged in");
+} else {
+    console.log("This user doesn't exist");
+}
